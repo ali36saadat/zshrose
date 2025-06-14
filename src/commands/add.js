92071fd4aadb3d4acd8ba45b968001c3fe65fb3d
ali <saadat@saafaa.ir>
2025-06-14 21:24:43 +0330

    Add add command

---
+++ src/commands/add.js
@@ -0,0 +1,23 @@
+import chalk from "chalk";
+import fs from "fs";
+import os from "os";
+import path from "path";
+
+const add = function (name, command) {
+    const zshrcPath = path.join(os.homedir(), ".zshrc");
+
+    const blockToAppend = `alias ${name} = "${command}"`;
+
+    try {
+        if (!fs.existsSync(zshrcPath)) {
+            process.exit(1);
+        }
+
+        fs.appendFileSync(zshrcPath, blockToAppend, "utf-8");
+        console.log("Added");
+    } catch (err) {
+        console.error(err.message);
+    }
+};
+
+export default add;
